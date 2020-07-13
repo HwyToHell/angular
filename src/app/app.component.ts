60d9fd8af3836a9b02b08138acab318a188a78ac
HwyToHell <holger.grzonka@web.de>
2020-07-13 22:38:22 +0200

    24 databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h1 {
+      color: maroon;
+    }
+  `]
 })
 export class AppComponent {
   title = 'first-app';
